60e65d33869d7952ab9d76e19a5621145b3b8d47
Michael Simmelbauer <mas@Michaels-MacBook-Pro.local>
2016-12-29 16:37:25 +0100

    Add styles to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,18 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+    <h3 className="text-center">About Component</h3>
+    <div className="media-object">
+    <div className="media-object-section">
+      <img src= "http://placeimg.com/200/200/people" />
+    </div>
+    <div className="media-object-section">
+      <h4>Dreams feel real while we're in them.</h4>
+      <p>I'm going to improvise. Listen, there's something you should know about me... about inception. An idea is like a virus, resilient, highly contagious. The smallest seed of an idea can grow. It can grow to define or destroy you.</p>
+    </div>
+    </div>
+  </div>
   )
 };
 
